cc021ce5efa0456bbc2c2c861459fc9511bdbea1
Dhanush Kumar <dk652003@gmail.com>
2024-02-12 12:30:14 +0530

    Added Edited Contact feature

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
-import com.bridgelabz.addressbook.AddressBook;
-import com.bridgelabz.addressbook.Contact;
 
 public class Main {
     public static void main(String[] args) {
-        AddressBook addressBook = new AddressBook();
-
-        Contact contact1 = new Contact("Dhanush", "Kumar", "466 Main Street", "Chennai", "Tamil Nadu", "12345", "12345", "xxx@email.com");
-        Contact contact2 = new Contact("Ram", "Josh", "456 sub Street", "Trichy", "Tamil Nadu", "67890", "67890", "yyy@email.com");
-        addressBook.addContactFromConsole();
-        addressBook.displayContacts();
-        contact1.displayContact();
-        contact2.displayContact();
     }
 }
---
+++ src/com/bridgelabz/addressbook/ContactAddress.java
@@ -0,0 +1,74 @@
+package com.bridgelabz.addressbook;
+
+public class ContactAddress {
+    String Firstname;
+    String Lastname;
+    String Address;
+    String City;
+    int Zip;
+    long Phnum;
+    String Email;
+
+    public ContactAddress(String firstname, String lastname, String address, String city, int zip, long phnum, String email) {
+        Firstname = firstname;
+        Lastname = lastname;
+        Address = address;
+        Zip = zip;
+        Phnum = phnum;
+        Email = email;
+    }
+    public String getFirstName(){
+        return Firstname;
+    }
+    public String getLastName(){
+        return Lastname;
+    }
+    public String getAddress(){
+        return Address;
+    }
+    public String getCity(){
+        return City;
+    }
+    public int getZip(){
+        return Zip;
+    }
+    public long getPhoneNum(){
+        return Phnum;
+    }
+    public String getEmail(){
+        return Email;
+    }
+    public void setFirstname(String firstname){
+        Firstname = firstname;
+    }
+    public void setLastName(String lastName){
+        Lastname = lastName;
+    }
+    public void setAddress(String address){
+        Address = address;
+    }
+    public void setCity(String city){
+        City = city;
+    }
+    public void setZip(int zip){
+        Zip = zip;
+    }
+    public void setPhoneNum(int phoneNum){
+        Phnum = phoneNum;
+    }
+    public void setEmail(String email){
+        Email = email;
+    }
+    @Override
+    public String toString() {
+        return "ContactAddress{" +
+                "Firstname='" + Firstname + '\'' +
+                ", Lastname='" + Lastname + '\'' +
+                ", Address='" + Address + '\'' +
+                ", City='" + City + '\'' +
+                ", Zip=" + Zip +
+                ", Phnum=" + Phnum +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
+}
